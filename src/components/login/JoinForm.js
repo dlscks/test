1f108e5752c22f8114f83e4933e2de1b8bda4b1f
dlscks <dlscks971026@gmail.com>
2023-02-14 21:43:08 +0900

    up

--- src/components/login/JoinForm.js
+++ src/components/login/JoinForm.js
@@ -12,7 +12,9 @@ const JoinForm = () => {
     passwordConfirm: "",
     email: "",
     nickName: "",
-    authRole: " ROLE_MEMBER",
+    birth: "",
+    gender: "",
+    authRole: "ROLE_MEMBER",
   });
 
   const onSubmit = async (e) => {
@@ -28,6 +30,8 @@ const JoinForm = () => {
           passwordConfirm: "",
           email: "",
           nickName: "",
+          birth: "",
+          gender: "",
           authRole: "ROLE_MEMBER",
         });
       })
@@ -35,68 +39,92 @@ const JoinForm = () => {
         navigator("/");
       })
       .catch((err) => {
-        console.error(err.message);
+        console.log(err.message);
       });
   };
 
   const handleValueChange = (e) => {
-    // radio 버튼에서는 e.preventDefault()를 하면 더블클릭을 해줘야 한다.
+    // radio 버튼에서는 preventDefault()를 하면 더블클릭을 해줘야 한다.
     // e.preventDefault();
     setMember({ ...member, [e.target.name]: e.target.value });
+    var p1 = document.getElementById("password").value;
+    var p2 = document.getElementById("passwordConfirm").value;
+    if (p1 == "" || )
   };
 
   return (
-    <div className="container">
+    <div>
       <form onSubmit={onSubmit}>
         <div className="container">
           <h1>회원가입</h1>
           <div className="form-group mb-1">
+            <span>아이디</span>
             <input
               type="text"
               className="form-control"
               name="username"
-              placeholder="아이디"
+              placeholder="영문과 숫자를 조합하여 4~12자 안으로 입력."
               onChange={handleValueChange}
             />
           </div>
           <div className="form-group mb-1">
+            <span>비밀번호</span>
             <input
               type="password"
               className="form-control"
               name="password"
-              placeholder="비밀번호"
+              placeholder="영어, 숫자, 특수문자를 조합하여 8~12자 안으로 입력"
               onChange={handleValueChange}
             />
           </div>
-
           <div className="form-group mb-1">
+            <span>비밀번호 확인</span>
             <input
               type="password"
               className="form-control"
-              name="password"
-              placeholder="비밀번호확인"
+              name="passwordConfirm"
+              placeholder="비밀번호 확인"
               onChange={handleValueChange}
             />
           </div>
-
           <div className="form-group mb-1">
+            <span>이메일</span>
             <input
               type="email"
               className="form-control"
               name="email"
-              placeholder="이메일"
+              placeholder="Email"
               onChange={handleValueChange}
             />
           </div>
           <div className="form-group mb-1">
+            <span>닉네임</span>
             <input
               type="nickName"
               className="form-control"
               name="nickName"
-              placeholder="닉네임"
+              placeholder="영어,한글,숫자 상관없이 2~7자 안으로 입력"
               onChange={handleValueChange}
             />
           </div>
+          <div className="form-group mb-1">
+            <span>출생년도</span>
+            <input
+              type="birth"
+              className="form-control"
+              name="birth"
+              placeholder="출생년도 4자리 입력 "
+              onChange={handleValueChange}
+            />
+          </div>
+          <label className="form-group mb-1">
+            <span>성별</span>
+            <br />
+            <input type="radio" name="gender" className="genchk" value="남" />남
+          </label>
+          <label className="form-group mb-1">
+            <input type="radio" name="gender" className="genchk" value="여" />여
+          </label>
           <hr className="my-3" />
           <div className="form-group mb-3 mb-1">
             <div
@@ -127,7 +155,7 @@ const JoinForm = () => {
                   name="authRole"
                   value="ROLE_MEMBER"
                   className="form-check-input"
-                  //   기본값으로 설정
+                  // 기본으로 사용하기 위해 defaultChecked={true} 사용
                   defaultChecked={true}
                 />
                 일반 사용자
